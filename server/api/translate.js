784c8354f90a3c76026d03ded8d4357968e2d392
beq <blakebequette@gmail.com>
2023-01-24 17:56:01 -0800

    delete failed tests

--- server/api/translate.js
+++ server/api/translate.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { exec } = require("child_process");
 
 router.get("/", async (req, res, next) => {
     res.json({translation: "Let's take a walk around the park"})
@@ -7,7 +8,7 @@ router.get("/", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
     const  { audioToTranslate } = req.body
     // req.user
-    const { languageFrom } = req.user
+    // const { languageFrom } = req.user
 
     const config = {
         languageFrom: "en",
@@ -22,6 +23,18 @@ router.post("/", async (req, res, next) => {
     //         textReponse: "a;sldkfj;owiejrfo "
     //     }
     // )
+    res.json({
+        langFrom: "en",
+        langTo: "es",
+        textResponse: "The quick brown fox jumped over the lazy dog."
+    })
+})
+
+router.post("/test/", async (req, res, next) => {
+    exec("argos-translate --from en --to es \"I have a lot of food\"", (err, stdout, stdin) => {
+        if (err) console.log(err)
+        else res.json({textResponse: stdout.slice(0, -2)})
+    })
 })
 
 module.exports = router
