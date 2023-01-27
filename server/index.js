da7f20e299c5ff98e08d7639a2f7e0be2e45ee0a
Jamison Pereira <113544207+jamisonpereira@users.noreply.github.com>
2023-01-27 17:55:21 +0100

    audio stream + translate working (before refactor)

--- server/index.js
+++ server/index.js
@@ -1,46 +1,58 @@
 const port = process.env.PORT || 3000;
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const { Server } = require('socket.io');
+// const express = require('express');
+import express from 'express';
+// const path = require('path');
+// const morgan = require('morgan');
+// const { Server } = require('socket.io');
+// const AudioConversion = require('./translateEngine/AudioConversion');
+// const translate_from_file = require('../google');
+import { Server } from 'socket.io';
+import { AudioConversion } from './translateEngine/AudioConversion.js';
+// import { translate_from_file } from '../google.js';
 
 const app = express();
 
 // static middleware
-app
-  .use(express.static(path.join(__dirname, '..', 'public')))
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use(morgan('dev'));
+// app
+//   .use(express.static(path.join(__dirname, '..', 'public')))
+//   .use(express.json())
+//   .use(express.urlencoded({ extended: true }))
+//   .use(morgan('dev'));
 
-app.use('/api', require('./api'));
+// app.use('/api', require('./api'));
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
-// Error handling END - WEAR
-app.use((err, req, res, next) => {
-  //console.log(`ERROR: Request: ${req.path}, Received: ${err.status} ${err.message.slice(0,30)}`)
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
-});
+// // Error handling END - WEAR
+// app.use((err, req, res, next) => {
+//   //console.log(`ERROR: Request: ${req.path}, Received: ${err.status} ${err.message.slice(0,30)}`)
+//   res.status(err.status || 500).send(err.message || 'Internal server error.');
+// });
 
 const server = app.listen(port, () => console.log(`listening on port ${port}`));
 
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-  console.log('connected to ', socket.id);
+  console.log('connected to (server): ', socket.id);
 
   // socket.on("hello", data => console.log(data))
 
   socket.emit('send-it', 'ready to receive');
 
-  socket.on('audio', (data) => {
-    console.log(data);
-    // const answer = audioConversionAndTranslateHandler(data)
-    socket.emit('answer', answer);
+  socket.on('audio', async (data) => {
+    console.log('data received (server) ');
+    // const convertedAudio = AudioConversion(data);
+    const answer = await AudioConversion(data);
+    // const answer = translate_from_file();
+    // console.log('answer: ', answer);
+    console.log('audio converted');
+    // socket.emit('answer', answer);
     // closes connection
+    socket.disconnect();
+    console.log('connection closed');
   });
 });
 
