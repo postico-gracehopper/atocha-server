eab4ac2cb9afd2f8e47a8c8b5d676177829e3b6e
beq <blakebequette@gmail.com>
2023-01-31 17:42:37 -0800

    add translate session logging - placeholder

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const { Server } = require('socket.io');
 const fs = require("fs")
-const translateLocalFLAC = require("./translateEngine/translateSession")
+const GoogleTranslateSession = require("./translateEngine/translateSession")
 const AudioConversion = require("./translateEngine/AudioConversion")
 
 const app = express();  
@@ -17,20 +17,41 @@ const io = new Server(server)
 
 io.on("connection", (socket) => {
   console.log("conneted to socket", socket.id)
-
+  
   socket.on('audio', async (data) =>{
-    if (data.fileFormat === "flac"){
-      fs.writeFileSync('./audio/serverSaved.flac', data.audioData, 'base64', {flag: 'w'})
-      translateLocalFLAC("./audio/serverSaved.flac", data.langSource, data.langTarget, true, socket)
-
-    } else if (data.fileFormat === 'm4a') {
-      console.log('received an m4a')
-      const tempFlacPath = await AudioConversion(data.audioData, './audio/tempM4A.m4a', './audio/serverSaved.flac')
-      translateLocalFLAC(tempFlacPath, data.langSource, data.langTarget, true, socket)
-    }
-
+    const startTime = Date.now()
+    const tempFlacPath = await AudioConversion(data.audioData, './audio/tempM4A.m4a', './audio/serverSaved.flac')
+    
+    var outTxt = ''
+    const GTranslator = new GoogleTranslateSession(data.langSource, data.langTarget, true)
+    const writeStream2Google = GTranslator.googleStream2SocketPlusLog(socket, (finalTranslation) => {
+      return makeSessionRecord (socket.id, data.langSource, data.langTarget, data.audioData, finalTranslation.translation, startTime)
+    })
+    
+    let chunks = [GTranslator.initialRequest]
+    fs.createReadStream(tempFlacPath).on('data', (data) => {
+      chunks.push(GTranslator.requestify(data))
+    }).on('close', () => {
+      chunks.map(chunk => {
+        writeStream2Google.write(chunk)
+      })
+      writeStream2Google.end()
+    })
+    
   })
-
+  
 })
 
 
+function makeSessionRecord(user, sourceLang, targetLang, inputFile, outputText, startTime){
+  console.log(JSON.stringify(
+    {
+      user,
+      sourceLang,
+      targetLang,
+      inputFile: inputFile.slice(0,20), 
+      outputText,
+      elapsedTime: Date.now() - startTime
+    }
+  ))
+}
