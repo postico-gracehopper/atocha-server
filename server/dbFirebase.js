51a9ed8ba35c51acf8d1139dd414d7250a5706b8
beq <blakebequette@gmail.com>
2023-02-02 10:14:50 -0800

    clean up packages and dbFirebase file

--- server/dbFirebase.js
+++ server/dbFirebase.js
@@ -4,18 +4,6 @@
 //   projectId: 'atocha-c5fca',
 //   keyFilename: './fireBaseConfig.json',
 // })
-
-// const docRef = db.collection('users').doc('alovelace');
-
-// docRef.set({
-//   first: 'Ada',
-//   last: 'Lovelace',
-//   born: 1815
-// });
-
-// module.exports = db
-
-
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
